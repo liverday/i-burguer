def0036c17e6eef5e4b0c821481250da1557b9dd
liverday <vitor.medeiro10@gmail.com>
2019-01-05 21:06:46 -0200

    tabs page

--- src/app/tabs/tabs-page/tabs-page.component.ts
+++ src/app/tabs/tabs-page/tabs-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { BurguerEmitterService } from '../../burguers/burguers.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tabs-page',
   templateUrl: './tabs-page.component.html',
-  styleUrls: ['./tabs-page.component.scss']
+  styleUrls: ['./tabs-page.component.scss'],
+  providers: [BurguerEmitterService]
 })
 export class TabsPageComponent implements OnInit {
+  public cartCount: number = 0;
+  private subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private burguerEmitter: BurguerEmitterService) {
+    this.subscription = this.burguerEmitter.cartAddedCount$.subscribe(this.onCartAddedCount)
   }
 
+  ngOnInit() {
+
+  }
+
+  private onCartAddedCount = (): void => {
+    this.cartCount++;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsPageComponent } from './tabs-page/tabs-page.component';
-import { BurguersModule } from '../burguers/burguers.module';
-import { CartModule } from '../cart/cart.module';
 import { TabsRoutingModule } from './tabs-routing.module';
 import { IonicModule } from '@ionic/angular';
+import { BurguerEmitterService } from '../burguers/burguers.service';
 
 @NgModule({
   declarations: [TabsPageComponent],
